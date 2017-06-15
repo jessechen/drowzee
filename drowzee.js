f4fc5489c6c6dbfabc9551516557ba9f87fd4fe9
Jesse Chen <jesse.z.chen@gmail.com>
2017-06-14 20:06:12 -0400

    Continue code cleanup

--- drowzee.js
+++ drowzee.js
@@ -1,25 +1,32 @@
 const TAU = 2 * Math.PI;
-// 10000 arbitrary units of time is one perfect loop.
+// This many arbitrary units of time is one perfect loop.
 const AUTS_PER_LOOP = 10000;
 const AUTS_PER_SECOND = 500;
 const LARGEST_ORBIT_RADIUS = 386;
 
+// The divisor starts at this number and increases by 1 for each orbit closer to the center.
+const OUTERMOST_DIVISOR = 2;
+// Don't draw any orbits smaller than this one.
+const INNERMOST_ORBIT_INDEX = 3;
+
 const orbitCount = 12;
 
-const orbitRadius = function(index) {
-    return index / (orbitCount + 3) * LARGEST_ORBIT_RADIUS;
-};
-
 const getX = function(index, auts) {
-    const radius = orbitRadius(index);
-    const actualRevolutions = auts / AUTS_PER_LOOP * (orbitCount + 5 - index);
-    return radius * Math.sin(actualRevolutions * TAU);
+    return polarToCartesian(index, auts, Math.sin);
 };
 
 const getY = function(index, auts) {
+    return polarToCartesian(index, auts, Math.cos);
+};
+
+const polarToCartesian = function(index, auts, fn) {
     const radius = orbitRadius(index);
-    const actualRevolutions = auts / AUTS_PER_LOOP * (orbitCount + 5 - index);
-    return radius * Math.cos(actualRevolutions * TAU);
+    const revolutions = auts / AUTS_PER_LOOP * (orbitCount + INNERMOST_ORBIT_INDEX + OUTERMOST_DIVISOR - index);
+    return radius * fn(revolutions * TAU);
+};
+
+const orbitRadius = function(index) {
+    return index / (orbitCount + INNERMOST_ORBIT_INDEX) * LARGEST_ORBIT_RADIUS;
 };
 
 const draw = function(auts) {
@@ -32,7 +39,7 @@ const surface = d3.select("svg");
 
 const orbits = surface
     .selectAll(".orbit")
-    .data(_.range(3, orbitCount + 3))
+    .data(_.range(INNERMOST_ORBIT_INDEX, orbitCount + INNERMOST_ORBIT_INDEX))
     .enter().append("circle")
         .classed("orbit", true)
         .attr("cx", 0)
@@ -43,7 +50,7 @@ const orbits = surface
 
 const planets = surface
     .selectAll(".planet")
-    .data(_.range(3, orbitCount + 3))
+    .data(_.range(INNERMOST_ORBIT_INDEX, orbitCount + INNERMOST_ORBIT_INDEX))
     .enter().append("circle")
         .classed("planet", true)
         .attr("cx", getX)
