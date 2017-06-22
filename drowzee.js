55e1756506c636e30e5a5d4c1db2bb927a125c83
Jesse Chen <jesse.z.chen@gmail.com>
2017-06-21 23:00:50 -0400

    Huge mess but + and - work

--- drowzee.js
+++ drowzee.js
@@ -9,34 +9,38 @@ const LARGEST_ORBIT_RADIUS = 386;
 const OUTERMOST_REVOLUTIONS_PER_LOOP = 2;
 // Don't draw any orbits smaller than this one.
 const INNERMOST_ORBIT_INDEX = 3;
+const MIN_ORBITS = 2;
+const INITIAL_ORBITS = 12;
+const MAX_ORBITS = 20;
 
-const orbitCount = 12;
-let startAuts = 0;
+let planetIndices = Array.from(Array(INITIAL_ORBITS).keys());
+let initialAuts = 0;
+let auts = 0;
 let playing = false;
 
-const getX = function(index, auts) {
-    const [r, theta] = getPolarCoordinates(index, auts);
+const getX = function(index) {
+    const [r, theta] = getPolarCoordinates(index);
     return r * Math.sin(theta);
 };
 
-const getY = function(index, auts) {
-    const [r, theta] = getPolarCoordinates(index, auts);
+const getY = function(index) {
+    const [r, theta] = getPolarCoordinates(index);
     return r * Math.cos(theta);
 };
 
-const getPolarCoordinates = function(index, auts) {
+const getPolarCoordinates = function(index) {
     const radius = orbitRadius(index);
-    const speed = orbitCount + OUTERMOST_REVOLUTIONS_PER_LOOP - index;
+    const speed = planetIndices.length + OUTERMOST_REVOLUTIONS_PER_LOOP - index;
     const revolutions = auts / AUTS_PER_LOOP * speed;
     return [radius, revolutions * TAU];
 };
 
 const getColor = function(index) {
-    return d3.hcl(index / orbitCount * 360, 50, 50);
+    return d3.hcl(index / planetIndices.length * 360, 50, 50);
 };
 
 const orbitRadius = function(index) {
-    return (index + INNERMOST_ORBIT_INDEX) / (orbitCount + INNERMOST_ORBIT_INDEX) * LARGEST_ORBIT_RADIUS;
+    return (index + INNERMOST_ORBIT_INDEX) / (planetIndices.length + INNERMOST_ORBIT_INDEX) * LARGEST_ORBIT_RADIUS;
 };
 
 const togglePlay = function(value) {
@@ -47,7 +51,7 @@ const togglePlay = function(value) {
         playing = true;
         d3.timer(tick);
     } else {
-        startAuts = parseInt(timeSlider.property("value"), 10);
+        initialAuts = auts;
         playing = false;
     }
     playButton.text(playing ? "❚❚ Pause" : "▶ Play");
@@ -55,10 +59,16 @@ const togglePlay = function(value) {
 
 const seek = function() {
     togglePlay(false);
-    draw(startAuts);
+    let inputValue = parseInt(timeSlider.property("value"), 10);
+    if (initialAuts != inputValue) {
+        initialAuts = inputValue;
+        auts = inputValue;
+        draw(initialAuts);
+    }
 };
 
 const parse = function() {
+    togglePlay(false);
     let inputValue = parseInt(timeInput.property("value"), 10);
     if (isNaN(inputValue) || inputValue < 0) {
         inputValue = 0;
@@ -66,54 +76,84 @@ const parse = function() {
         inputValue = AUTS_PER_LOOP - 1;
     }
     timeInput.property("value", inputValue);
-    if (parseInt(timeSlider.property("value"), 10) != inputValue) {
-        timeSlider.property("value", inputValue);
-        seek();
+    if (initialAuts != inputValue) {
+        initialAuts = inputValue;
+        auts = inputValue;
+        draw(initialAuts);
     }
 };
 
-const draw = function(auts) {
-    planets
-        .attr("cx", o => { return getX(o, auts) })
-        .attr("cy", o => { return getY(o, auts) });
+const add = function() {
+    if (planetIndices.length < MAX_ORBITS) {
+        planetIndices.push(planetIndices.length);
+        draw();
+    }
+};
+
+const remove = function() {
+    if (planetIndices.length > MIN_ORBITS) {
+        planetIndices.pop();
+        draw();
+    }
+};
+
+const draw = function() {
+    update(planetIndices);
     timeSlider.property("value", Math.floor(auts));
     timeInput.property("value", Math.floor(auts));
 };
 
 const tick = function(time) {
     if (playing) {
-        auts = (startAuts + (time / 1000) * (AUTS_PER_LOOP / SECONDS_PER_LOOP)) % AUTS_PER_LOOP;
-        draw(auts);
+        auts = (initialAuts + (time / 1000) * (AUTS_PER_LOOP / SECONDS_PER_LOOP)) % AUTS_PER_LOOP;
+        draw();
     }
     return !playing;
 };
 
 const surface = d3.select("svg");
 
-const orbits = surface
-    .selectAll(".orbit")
-    .data(Array.from(Array(orbitCount).keys()))
-    .enter().append("circle")
-        .classed("orbit", true)
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .attr("r", orbitRadius)
-        .attr("stroke", getColor)
-        .attr("fill-opacity", 0);
+const update = function(planetIndices) {
+    var orbits = surface
+        .selectAll(".orbit")
+        .data(planetIndices);
 
-const planets = surface
-    .selectAll(".planet")
-    .data(Array.from(Array(orbitCount).keys()))
-    .enter().append("circle")
-        .classed("planet", true)
-        .attr("cx", getX)
-        .attr("cy", getY)
-        .attr("r", 8)
-        .attr("stroke", "black")
-        .attr("fill", "#22c");
+    orbits.enter().append("circle")
+            .classed("orbit", true)
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("fill-opacity", 0)
+        .merge(orbits)
+            .attr("r", orbitRadius)
+            .attr("stroke", getColor);
+    orbits.exit().remove();
+
+    var planets = surface
+        .selectAll(".planet")
+        .data(planetIndices);
+
+    planets.enter().append("circle")
+            .classed("planet", true)
+            .attr("r", 8)
+            .attr("stroke", "black")
+            .attr("fill", "#22c")
+        .merge(planets)
+            .attr("cx", getX)
+            .attr("cy", getY);
+
+    planets.exit().remove();
+};
 
 const playButton = d3.select(".play-button")
     .on("click", togglePlay);
+
+const addButton = d3.select(".add-button")
+    .text("+")
+    .on("click", add);
+
+const removeButton = d3.select(".remove-button")
+    .text("-")
+    .on("click", remove);
 
 const timeSlider = d3.select(".time-slider")
     .attr("min", 0)
