49cd5ced8afb6d8444af05ec410878712f01b2aa
Jesse Chen <jesse.z.chen@gmail.com>
2025-04-15 10:46:42 -0400

    Pull in basics from berenice, translated to svelte 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Orbiter from './Orbiter.svelte';
+
+    const CYCLE_TIME: number = 10_000;
+
+    let time: number = 0;
+</script>
+
+<svelte:head>
+    <title>Drowzee</title>
+    <meta name="description" content="Drowzee" />
+</svelte:head>
+<main>
+    <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+        <Orbiter color="red" orbits={3} distance={100} {time} />
+        <Orbiter color="orange" orbits={2} distance={200} {time} />
+        <Orbiter color="yellow" orbits={1} distance={300} {time} />
+    </svg>
+</main>
+<footer>
+	<input class="slider" type="range" id="time" min="0" max="10000" bind:value={time} />
+</footer>
+
+<style>
+    main {
+		width: calc(100vmin - 50px);
+		height: calc(100vmin - 50px);
+	}
+
+	svg {
+		width: 100%;
+		height: 100%;
+	}
+
+	footer {
+		height: 50px;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.slider {
+		width: 100%;
+		margin: 10px;
+	}
+</style>
---
+++ src/routes/Orbiter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let {color, orbits, distance, time} = $props();
+
+    const TAU: number = 2 * Math.PI;
+    const CYCLE_TIME: number = 10_000;
+
+    let cx = $derived(Math.cos((time * TAU * orbits) / CYCLE_TIME) * distance + 500);
+    let cy = $derived(Math.sin((time * TAU * orbits) / CYCLE_TIME) * distance + 500);
+</script>
+
+<circle cx="500" cy="500" r={distance} stroke="#2d3359" fill="none" />
+<circle {cx} {cy} r=20 fill={color} />
