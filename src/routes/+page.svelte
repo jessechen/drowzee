0fcd2839f8c11a4c77c49ce9716491af7755e8a5
Jesse Chen <jesse.z.chen@gmail.com>
2025-04-22 14:05:44 -0400

    Specify the index as the key for the collection of orbiters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,7 +55,7 @@
 </svelte:head>
 <main>
     <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-        {#each { length: orbiterCount }, index}
+        {#each { length: orbiterCount }, index (index)}
             <Orbiter {index} count={orbiterCount} time={cycleProgress} />
         {/each}
     </svg>
